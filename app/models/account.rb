4464be427b09b6f4d0510c080980ed7e6df2f8d2
Nate Borrebach <nate@hubbubhealth.com>
2015-03-09 16:06:43 -0700

    Replace hardwired example settings with env vars.

--- app/models/account.rb
+++ app/models/account.rb
@@ -5,29 +5,14 @@ class Account < ActiveRecord::Base
     settings = OneLogin::RubySaml::Settings.new
 
     # Example settings data, replace this values!
-    settings.assertion_consumer_service_url = "http://localhost:3000/saml/acs"
-    settings.assertion_consumer_logout_service_url = "http://localhost:3000/saml/logout"
-    settings.issuer                         = "http://localhost:3000/saml/metadata"
-    settings.idp_entity_id                  = "https://app.onelogin.com/saml/metadata/<onelogin-app-id>"
-    settings.idp_sso_target_url             = "https://app.onelogin.com/trust/saml2/http-post/sso/<onelogin-app-id>"
-    settings.idp_slo_target_url             = "https://app.onelogin.com/trust/saml2/http-redirect/slo/<onelogin-app-id>"
-    settings.idp_cert                       = "-----BEGIN CERTIFICATE-----
-MIICbjCCAdegAwIBAgIBADANBgkqhkiG9w0BAQ0FADBUMQswCQYDVQQGEwJ1czET
-MBEGA1UECAwKQ2FsaWZvcm5pYTEVMBMGA1UECgwMT25lbG9naW4gSW5jMRkwFwYD
-VQQDDBBhcHAub25lbG9naW4uY29tMB4XDTE0MDkxMTE1MDUxMVoXDTE1MDkxMTE1
-MDUxMVowVDELMAkGA1UEBhMCdXMxEzARBgNVBAgMCkNhbGlmb3JuaWExFTATBgNV
-BAoMDE9uZWxvZ2luIEluYzEZMBcGA1UEAwwQYXBwLm9uZWxvZ2luLmNvbTCBnzAN
-BgkqhkiG9w0BAQEFAAOBjQAwgYkCgYEAsezUGLSiNMXg80EZMMzvXH43f07a1plU
-mm1poYvVfgSICTqhEUuA0x4w9w/K4BegO07GVkUjNCrvtJEqq4FMDbHj2VfOCMHx
-lYi52/ELXKe6ALSm48y5BG9fd1kGHBqBg741KpMvDkmAbX1sLq5reAjOccIDme2d
-lLD8tQ8y0IMCAwEAAaNQME4wHQYDVR0OBBYEFP3cLGEyby7TBXweK0SeFrvnRoHL
-MB8GA1UdIwQYMBaAFP3cLGEyby7TBXweK0SeFrvnRoHLMAwGA1UdEwQFMAMBAf8w
-DQYJKoZIhvcNAQENBQADgYEAS3vJKRFGjvxOHHNJR77wYlxSSbyb9vmWOVBzaTMd
-5TFfoSDxuP9RskRjSrez/63WON6tdYr/mJNSNB70ZAic824Y9feQ/kBaCswI+Pgp
-b6sFIh4CrY4yCEkBPD8jXFCeJMq+2AqshbITpiu7WF1RCAp/sRAO56giP/B4l0o0
-Tc0=
------END CERTIFICATE-----"
-    # or settings.idp_cert_fingerprint           = "3B:05:BE:0A:EC:84:CC:D4:75:97:B3:A2:22:AC:56:21:44:EF:59:E6"
+    settings.assertion_consumer_service_url = "#{ENV['HOST_URL']}/saml/acs"
+    settings.assertion_consumer_logout_service_url = "#{ENV['HOST_URL']}/saml/logout"
+    settings.issuer                         = "#{ENV['HOST_URL']}/saml/metadata"
+    settings.idp_entity_id                  = ENV['IDP_ENTITY_ID']
+    settings.idp_sso_target_url             = ENV['IDP_SSO_TARGET_URL']
+    settings.idp_slo_target_url             = ENV['IDP_SLO_TARGET_URL']
+    settings.idp_cert                       = ENV['IDP_CERT'] if ENV['IDP_CERT'].present?
+    settings.idp_cert_fingerprint           = ENV['IDP_CERT_FINGERPRINT'] if ENV['IDP_CERT_FINGERPRINT'].present?
     settings.name_identifier_format         = "urn:oasis:names:tc:SAML:1.1:nameid-format:emailAddress"
     settings
   end
